ba830d7e1844696cb31107cc6d61171a811ef40e
peter nadel <peter.m.nadel@gmail.com>
2023-02-10 16:49:01 -0500

    added description

--- app.py
+++ app.py
@@ -8,6 +8,18 @@ import plotly.io as pio
 pio.templates.default = 'plotly'
 
 st.title("Ancient Greek Perseus Text Visualizer")
+st.write(
+    """
+    [Perseus](http://www.perseus.tufts.edu/hopper/) contains a wide range of Ancient Greek texts from Homer to the New Testament and beyond, 
+    yet the relationships between these texts remain under explored in a digital context. This tool, taking advantage of [Gensim's Doc2Vec model](https://radimrehurek.com/gensim/models/doc2vec.html),
+    renders indivdual Ancient Greek texts from the Perseus Digital Library into a form that can easily be compared in the 2 dimensional plot below. It allows researchers to directly compare
+    the works of different authors. 
+    
+    Using the drop down menu just below this description, you can select any author from the Perseus Digital Library and see how similar 
+    their works are to any other author. Too, selecting any work with the box select tool, just above the plot, will bring up the 
+    most similar works to it from the corpus. 
+    """
+)
 
 @st.cache(allow_output_mutation=True)
 def load_d2v():
@@ -25,7 +37,7 @@ def get_urn_cts(fname):
 options = st.multiselect(
     'Select author to visualize (listed in alphabetical order).',
     grc_df.author.sort_values().unique(),
-    ['Aeschylus', 'Aristophanes', 'Euripides', 'Sophocles']
+    ['Aeschylus', 'Aristophanes', 'Euripides', 'Sophocles', 'New Testament']
 )
 
 options = [f'^{n}$' for n in sorted(options)]
@@ -33,6 +45,8 @@ df = grc_df.loc[grc_df.author.str.contains('|'.join(options))]
 
 fig = px.scatter(df, x='x', y='y', color='author', hover_data=['title'])
 fig_selected = plotly_events(fig, select_event=True)
+
+topn = st.number_input('Choose the amount of similar documents', value=5)
 
 if st.button('Reset'):
     fig_selected = []
@@ -44,7 +58,7 @@ if len(fig_selected) > 0:
         selected_author = subset.author.to_list()[0]
         st.write(f'#### *{selected_title.strip()}*, by {selected_author.strip()}')
         st.write('<small><b>Most similar documents</b></small>',unsafe_allow_html=True)
-        res = grc_d2v.most_similar(title2index[selected_title], topn=5)
+        res = grc_d2v.most_similar(title2index[selected_title], topn=topn)
         rel = [(index2title[tup[0]], f'http://data.perseus.org/texts/urn:cts:greekLit:{get_urn_cts(g_notext.iloc[tup[0]]["filename"])}', g_notext.iloc[tup[0]]['author'],tup[1]) for tup in res]
         for i, r in enumerate(rel):       
             st.write(f'**Title**: *{r[0].strip()}*, by {r[2]} ({r[1]})')
